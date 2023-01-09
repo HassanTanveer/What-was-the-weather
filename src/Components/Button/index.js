4424171d16c962b4ee9f62237f973da5afe3c49b
Hassan Tanveer <hassan.tanveer@arbisoft.com>
2023-01-10 02:43:45 +0500

    Styled button component

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const Button = ({ text, onClick }) => {
   return (
-    <button onClick={onClick}>
+    <button
+      onClick={onClick}
+      className='
+        p-5 
+        ml-5
+        mr-5
+        mb-2
+        w-40
+        focus:ring 
+        bg-gray-300
+        hover:bg-blue-300
+        dark:bg-purple-900
+        dark:hover:bg-purple-800
+        active:bg-blue-400
+        focus:ring-violet-300
+        flex items-center shrink-0 justify-center
+        rounded-xl shadow-lg space-x-4 
+      '
+    >
       {text}
     </button>
   )
