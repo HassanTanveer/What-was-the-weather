1e50af87ce5c580b4de47c80e7bebcc7a9f83071
Hassan Tanveer <hassan.tanveer0097@gmail.com>
2023-05-06 20:17:56 +0500

    :sparkles: Added Particles in background

--- src/Containers/Homepage/index.js
+++ src/Containers/Homepage/index.js
@@ -1,4 +1,7 @@
 import React, { useContext } from 'react';
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadSnowPreset } from "tsparticles-preset-snow";
 
 import Button from '../../Components/Button';
 import Header from '../../Components/Header';
@@ -8,40 +11,73 @@ import { WeatherContext } from '../../Providers/WeatherProvider';
 
 const Homepage = () => {
   const { changeYear } = useContext(WeatherContext);
+  const particlesInit = useCallback(async engine => {
+    await loadSnowPreset(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+  }, []);
 
   return (
-    <div
-      id="HomePage"
-      className='
+    <>
+      <div
+        id="HomePage"
+        className='
+        relative
         font-mono 
-        bg-gradient-to-r  
-        from-white
-        to-blue-300
-        dark:bg-gradient-to-r 
-        dark:from-blue-800
-        dark:via-indigo-900
-        dark:to-slate-800
         text-black 
         dark:text-white 
         p-6 
         flex 
         items-center
         flex-col
+        z-50
       '
-    >
-      <div className="mb-7" >
-        <Header />
-      </div>
-      <div className='mb-5 flex items-center flex-row flex-wrap justify-center'>
-        {/* Add option to choose custom months and years ago */}
-        <Button text="1 year ago" onClick={() => changeYear(1)} />
-        <Button text="2 years ago" onClick={() => changeYear(2)} />
-        <Button text="5 years ago" onClick={() => changeYear(5)} />
-      </div>
-      <div>
-        <WeatherData date="10th January 2022" />
-      </div>
-    </div >
+      >
+        <div className="mb-7" >
+          <Header />
+        </div>
+        <div className='mb-5 flex items-center flex-row flex-wrap justify-center'>
+          {/* Add option to choose custom months and years ago */}
+          <Button text="1 year ago" onClick={() => changeYear(1)} />
+          <Button text="2 years ago" onClick={() => changeYear(2)} />
+          <Button text="5 years ago" onClick={() => changeYear(5)} />
+        </div>
+        <div>
+          <WeatherData date="10th January 2022" />
+        </div>
+      </div >
+      <Particles
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          preset: "snow",
+          fpsLimit: 60,
+          background: {
+            backgroundImage: "linear-gradient(to right, white, #BEE3F8)",
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1500,
+              },
+              value: 50,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+        }}
+      />
+    </>
   );
 };
 
