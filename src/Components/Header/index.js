81e13c0fd773bd57717c2bb4a5d6fdc391db8b13
Hassan Tanveer <hassan.tanveer0097@gmail.com>
2023-05-06 20:18:11 +0500

    :bug: Fixed Header title theme

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='font-black text-4xl text-center'>
+    <div className='font-black text-white text-4xl text-center'>
       What was the Weather?
     </div>
   );
